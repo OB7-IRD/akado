13d511d6315c411d2d3df403127cbd9bf7e92a3a
Tony Chemit <dev@tchemit.fr>
2023-04-04 11:46:27 +0200

    ird-common - Add nice methods in JDBCUtilities to use functional queries + clean code

--- ird-common/src/main/java/fr/ird/common/JDBCUtilities.java
+++ ird-common/src/main/java/fr/ird/common/JDBCUtilities.java
@@ -1,6 +1,14 @@
 package fr.ird.common;
 
+import io.ultreia.java4all.util.sql.SqlQuery;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Miscellaneous class utility methods for JDBC.
@@ -21,9 +29,9 @@ public class JDBCUtilities {
 
         for (Throwable e : ex) {
             if (e instanceof SQLException) {
-                if (ignoreSQLException(
+                if (!ignoreSQLException(
                         ((SQLException) e).
-                        getSQLState()) == false) {
+                                getSQLState())) {
 
                     e.printStackTrace(System.err);
                     System.err.println("SQLState: "
@@ -59,5 +67,29 @@ public class JDBCUtilities {
         // X0Y32: Jar file already exists in schema
         // 42Y55: Table already exists in schema
         return (sqlState.equalsIgnoreCase("X0Y32") || sqlState.equalsIgnoreCase("42Y55"));
+    }
+
+    public static <O> Optional<O> findFirstFirst(Connection connection, SqlQuery<O> query) {
+        try (PreparedStatement statement = query.prepareQuery(connection); ResultSet rs = statement.executeQuery()) {
+            if (rs.next()) {
+                return Optional.of(query.prepareResult(rs));
+            }
+        } catch (SQLException ex) {
+            JDBCUtilities.printSQLException(ex);
+        }
+        return Optional.empty();
+    }
+
+    public static <O> List<O> findList(Connection connection, SqlQuery<O> query) {
+        List<O> result = new LinkedList<>();
+        try (PreparedStatement statement = query.prepareQuery(connection); ResultSet rs = statement.executeQuery()) {
+            if (rs.next()) {
+                O row = query.prepareResult(rs);
+                result.add(row);
+            }
+        } catch (SQLException ex) {
+            JDBCUtilities.printSQLException(ex);
+        }
+        return result;
     }
 }
